0e2c4f37b88c44f06f25351ab512f7268a586f48
Pawel <pawel.smolik@gmail.com>
2024-09-28 14:56:19 +0200

    Add opentelemetry enable flag

--- WebApiExample/AppSettings.cs
+++ WebApiExample/AppSettings.cs
@@ -3,5 +3,6 @@
     public class AppSettings
     {
         public string Version { get; set; } = string.Empty;
+        public string OpenTelemetryEnable { get; set; } = string.Empty;
     }
 }
--- WebApiExample/Extensions/OpenTelemetryExtension.cs
+++ WebApiExample/Extensions/OpenTelemetryExtension.cs
@@ -10,24 +10,30 @@ namespace WebApiExample.Extensions
         const string serviceName = "WebApiExample";
         public static void AddOpenTelemetry(this IServiceCollection services, ILoggingBuilder logging, ConfigurationManager configuration)
         {
-            logging.AddOpenTelemetry(options =>
-            {
-                options.SetResourceBuilder(ResourceBuilder.CreateDefault()
-                    .AddService(serviceName))
-                    .AddConsoleExporter();
-            });
+            bool.TryParse(configuration.GetSection("AppSettings: OpenTelemetryEnable").Value, out bool openTelementyEnable);
 
-            services.AddOpenTelemetry()
-                  .ConfigureResource(resource => resource.AddService(serviceName))
-                  .WithTracing(tracing => tracing
-                      .AddAspNetCoreInstrumentation()
-                      .AddHttpClientInstrumentation()
-                      .AddEntityFrameworkCoreInstrumentation()
-                      .AddConsoleExporter()
-                      .AddZipkinExporter(opt => opt.Endpoint = new Uri("http://localhost:9411/api/v2/spans")))
-                  .WithMetrics(metrics => metrics
-                      .AddAspNetCoreInstrumentation()
-                      .AddConsoleExporter());
+            if (openTelementyEnable)
+            {
+
+                logging.AddOpenTelemetry(options =>
+                {
+                    options.SetResourceBuilder(ResourceBuilder.CreateDefault()
+                        .AddService(serviceName))
+                        .AddConsoleExporter();
+                });
+
+                services.AddOpenTelemetry()
+                      .ConfigureResource(resource => resource.AddService(serviceName))
+                      .WithTracing(tracing => tracing
+                          .AddAspNetCoreInstrumentation()
+                          .AddHttpClientInstrumentation()
+                          .AddEntityFrameworkCoreInstrumentation()
+                          .AddConsoleExporter()
+                          .AddZipkinExporter(opt => opt.Endpoint = new Uri("http://localhost:9411/api/v2/spans")))
+                      .WithMetrics(metrics => metrics
+                          .AddAspNetCoreInstrumentation()
+                          .AddConsoleExporter());
+            }
         }
     }
 }
